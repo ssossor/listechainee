5e5ee1a65664d4220872a23a515c3d7ddd4a6429
Ssor <erwanfranco13@gmail.com>
2024-01-11 16:02:48 +0100

    Initial Commit

---
+++ src/aff.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int afficheentier(int x) {
+printf("%d\n", x);
+return 0;
+}
+
---
+++ test/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "src/aff.h"
+
+int main (void) {
+afficheentier(4);
+return 0;
+}
