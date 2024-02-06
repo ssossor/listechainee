441f3afe8258127861049980519d5a29bfe39212
Ssor <erwanfranco13@gmail.com>
2024-02-06 09:20:18 +0100

    final commit

--- src/list.h
+++ src/list.h
@@ -5,11 +5,15 @@ struct element {
     struct element *pointer;
 };
 
-struct element * createlist(int x);
+struct element * createlist(int val);
 bool listisempty(struct element *list);
 int lenlist(struct element *list);
 void printlist (struct element *list);
-struct element * indexlist(struct element *list, int x);
-int valueindexlist(struct element *list, int x);
-void prependlist(struct element *list, int x);
-void appendlist(struct element *list, int x);
+void debuglist(struct element *list);
+struct element * indexlist(struct element *list, int idx);
+void prependlist(struct element *list, int val);
+void appendlist(struct element *list, int val);
+void deletefirst(struct element *list);
+void deletelast(struct element *list);
+void addelement(struct element *list, int idx, int val);
+void removeelement(struct element *list, int idx);
--- test/test.c
+++ test/test.c
@@ -7,8 +7,15 @@ int main (void) {
     struct element *testlist = createlist(7);
     appendlist(testlist, 8);
     appendlist(testlist, 9);
-    prependlist(testlist, 6);
     prependlist(testlist, 5);
+    addelement(testlist, 1, 6);
+
+    // testlist = [5, 6, 7, 8, 9]
+
+    printlist(testlist);
+
+    removeelement(testlist, 0);
+
     printlist(testlist);
     
     return 0;
